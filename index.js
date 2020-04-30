b3f41b419cc3f97767695943da87b488bff62d2a
Steffen Kühne <stekhn@gmail.com>
2020-04-30 17:01:19 +0200

    Add data fetching and updating

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
 const dateString = date.toLocaleDateString('de-DE', dateOptions);
 
 exports.datawrapperUpdater = async function (req, res) {
-  for (const id of charts) {
-    const update = await updateChart(id);
-    const publish = await publishChart(id);
+  for (const chart of charts) {
+    const { id, csvUrl } = chart;
 
-    if (update.error || update.status === 'error') {
-      console.error(`Error updating chart ${id}:`, JSON.stringify(update));
+    const csvData = await getCsv(csvUrl);
+    const dataStatus = await updateData(id, csvData);
+    const metaStatus = await updateMeta(id);
+    const publishStatus = await publishChart(id);
+
+    if (dataStatus.error) {
+      console.error(`Error updating data for chart ${id}:`, JSON.stringify(dataStatus));
     }
 
-    if (publish.error || publish.status === 'error') {
-      console.error(`Error publishing chart ${id}:`, JSON.stringify(publish));
+    if (metaStatus.error || metaStatus.status === 'error') {
+      console.error(`Error updating meta data for chart ${id}:`, JSON.stringify(metaStatus));
+    }
+
+    if (publishStatus.error || publishStatus.status === 'error') {
+      console.error(`Error publishing chart ${id}:`, JSON.stringify(publishStatus));
     }
   }
 
@@ -25,7 +33,24 @@ exports.datawrapperUpdater = async function (req, res) {
   }
 };
 
-async function updateChart(id) {
+async function getCsv(csvUrl) {
+  return fetch(csvUrl, {
+    method: 'GET'
+  }).then(res => res.text());
+}
+
+async function updateData(id, csv) {
+  return fetch(`https://api.datawrapper.de/v3/charts/${id}/data`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${apiKey}`,
+      'Content-Type': 'text/csv'
+    },
+    body: csv
+  }).then(res => res.text());
+}
+
+async function updateMeta(id) {
   return fetch(`https://api.datawrapper.de/v3/charts/${id}`, {
     method: 'PATCH',
     headers: {
