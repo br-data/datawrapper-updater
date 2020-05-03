bd16f557583e6758ad14c4a2ff96f46af07387ba
Steffen Kühne <stekhn@gmail.com>
2020-05-03 19:16:44 +0200

    Add chart overview HTML generator

---
+++ docs/generator.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const { charts } = require('../config.json');
+
+let toc = '';
+
+charts.forEach(chart => {
+  toc += `
+    <li><a href="#id-${chart.id}">${chart.title}</a></li>`;
+});
+
+let sections = '';
+
+charts.forEach(chart => {
+  const iframe = `
+    <iframe title="${chart.title}" aria-label="chart" id="datawrapper-chart-${chart.id}" src="https://datawrapper.dwcdn.net/${chart.id}" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; max-width: 730px; border: none;" height="500"></iframe>`;
+
+  sections += `
+  <section id="id-${chart.id}">
+    ${iframe.trim()}
+    <textarea readonly>${iframe.trim()}</textarea>
+    <p><a href="https://app.datawrapper.de/chart/${chart.id}/visualize#refine-the-chart">In Datawrapper bearbeiten</a></p>
+  </section>
+  `;
+});
+
+let html = `
+  <h1>Übersicht: Corona-Grafiken</h1>
+  <ul>
+    ${toc.trim()}
+  </ul>
+  ${sections.trim()}
+`;
+
+fs.writeFileSync('./template.html', html);
