a26c9b6491948e265c62b4e89177e9cb59959a49
Steffen Kühne <stekhn@gmail.com>
2020-10-22 14:51:07 +0200

    Fix data URL for preview

--- docs/generator.js
+++ docs/generator.js
@@ -18,7 +18,7 @@ charts.forEach(chart => {
   <section id="id-${chart.id}">
     ${iframe.trim()}
     <textarea readonly>${iframe.trim()}</textarea>
-    <p><a href="https://app.datawrapper.de/chart/${chart.id}/visualize#refine-the-chart">In Datawrapper bearbeiten</a> | <a href="https://datawrapper.dwcdn.net/${chart.id}/">Vorschau öffnen</a> | <a href="https://datawrapper.dwcdn.net/${chart.csvUrl}/">Daten anschauen</a></p>
+    <p><a href="https://app.datawrapper.de/chart/${chart.id}/visualize#refine-the-chart">In Datawrapper bearbeiten</a> | <a href="https://datawrapper.dwcdn.net/${chart.id}/">Vorschau öffnen</a> | <a href="${chart.csvUrl}">Daten anschauen</a></p>
   </section>
   `;
 });
