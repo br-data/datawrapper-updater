9476672a85e64cc23f07e935526474319be8408f
Steffen Kühne <stekhn@gmail.com>
2021-08-25 15:55:25 +0200

    Add new charts and grid layout

--- docs/generator.js
+++ docs/generator.js
@@ -4,31 +4,36 @@ const { charts } = require('../config.json');
 let toc = '';
 
 charts.forEach(chart => {
-  toc += `
-    <li><a href="#id-${chart.id}">${chart.title}</a></li>`;
+  toc += `<li><a href="#id-${chart.id}">${chart.title}</a></li>`;
 });
 
-let sections = '';
+let divs = '';
 
 charts.forEach(chart => {
   const iframe = `
     <iframe title="${chart.title}" aria-label="Interaktives Diagramm" id="datawrapper-chart-${chart.id}" src="https://datawrapper.dwcdn.net/${chart.id}" scrolling="no" frameborder="0" style="width: 100%; border: none;" height="${chart.height || 400}" width="${chart.width || 680}"></iframe>`;
 
-  sections += `
-  <section id="id-${chart.id}">
-    ${iframe.trim()}
-    <textarea readonly>${iframe.trim()}</textarea>
-    <p><a href="https://app.datawrapper.de/chart/${chart.id}/visualize#refine-the-chart">In Datawrapper bearbeiten</a> | <a href="https://datawrapper.dwcdn.net/${chart.id}/">Vorschau öffnen</a> | <a href="${chart.csvUrl}">Daten anschauen</a></p>
-  </section>
-  `;
+  divs += `
+      <div id="id-${chart.id}">
+        ${iframe.trim()}
+        <textarea readonly>${iframe.trim()}</textarea>
+        <p><a href="https://app.datawrapper.de/chart/${chart.id}/visualize#refine-the-chart">In Datawrapper bearbeiten</a> | <a href="https://datawrapper.dwcdn.net/${chart.id}/">Vorschau öffnen</a> | <a href="${chart.csvUrl}">Daten anschauen</a></p>
+      </div>`;
 });
 
 let html = `
-  <h1>Übersicht: Corona-Grafiken</h1>
-  <ul>
-    ${toc.trim()}
-  </ul>
-  ${sections.trim()}
-`;
+  <body>
+    <header>
+      <h1>Übersicht: Corona-Grafiken</h1>
+      <nav>
+        <ul>
+          ${toc.trim()}
+        </ul>
+      </nav>
+    </header>
+    <section>
+      ${divs.trim()}
+    </section>
+  </body>`;
 
 fs.writeFileSync(`${__dirname}/template.html`, html);
