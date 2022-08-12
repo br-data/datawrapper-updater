dc315b77fbf20a23d95732ced5cb457cee7b3c7e
Steffen Kühne <stekhn@gmail.com>
2022-08-12 13:28:35 +0200

    Get Datawrapper API key from ENV #12

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const charts = require("./charts.json");
+const apiKey = process.env.DATAWRAPPER_API_KEY;
 
-const { apiKey, charts } = require('./config.json');
+(async function init() {
+  if (!apiKey) {
+    console.error("Error: DATAWRAPPER_API_KEY is missing.");
+  } else if (!charts) {
+    console.error("Error: Chart configuration is missing or corrupted.");
+  } else {
+    await updateChart();
+  }
+})();
 
-exports.datawrapperUpdater = async function (req, res) {
+async function updateChart() {
   for (const chart of charts) {
     const { id, description, csvUrl } = chart;
 
@@ -28,7 +38,7 @@ exports.datawrapperUpdater = async function (req, res) {
       if (description) {
         const updateMetaStatus = await updateMeta(id, description, dateString);
 
-        if (updateMetaStatus.error || updateMetaStatus.status === 'error') {
+        if (updateMetaStatus.error || updateMetaStatus.status === "error") {
           console.error(
             `Error updating meta data for chart ${id}:`,
             JSON.stringify(updateMetaStatus)
@@ -37,7 +47,7 @@ exports.datawrapperUpdater = async function (req, res) {
       }
 
       const publishStatus = await publishChart(id);
-      if (publishStatus.error || publishStatus.status === 'error') {
+      if (publishStatus.error || publishStatus.status === "error") {
         console.error(
           `Error publishing chart ${id}:`,
           JSON.stringify(publishStatus)
@@ -45,15 +55,11 @@ exports.datawrapperUpdater = async function (req, res) {
       }
     }
   }
-
-  if (res && res.send) {
-    res.send();
-  }
-};
+}
 
 async function getCsv(id, csvUrl) {
   return fetch(csvUrl, {
-    method: 'GET',
+    method: "GET",
   })
     .then((res) => res.text())
     .catch(() => ({
@@ -63,10 +69,10 @@ async function getCsv(id, csvUrl) {
 
 async function updateData(id, csv) {
   return fetch(`https://api.datawrapper.de/v3/charts/${id}/data`, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
       Authorization: `Bearer ${apiKey}`,
-      'Content-Type': 'text/csv',
+      "Content-Type": "text/csv",
     },
     body: csv,
   }).then((res) => res.text());
@@ -74,10 +80,10 @@ async function updateData(id, csv) {
 
 async function updateMeta(id, description, dateString) {
   return fetch(`https://api.datawrapper.de/v3/charts/${id}`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: {
       Authorization: `Bearer ${apiKey}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       metadata: {
@@ -90,8 +96,8 @@ async function updateMeta(id, description, dateString) {
 }
 
 async function publishChart(id) {
-  return fetch(`https://api.datawrapper.de/charts/${id}/publish`, {
-    method: 'POST',
+  return fetch(`https://api.datawrapper.de/v3/charts/${id}/publish`, {
+    method: "POST",
     headers: {
       Authorization: `Bearer ${apiKey}`,
     },
